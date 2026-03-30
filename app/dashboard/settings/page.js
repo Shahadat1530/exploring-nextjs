dcbc3acb0bd471402c81c7b3e39b96156c011d0f
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2026-03-30 09:47:59 +0600

    exploring how to define routes

---
+++ app/dashboard/settings/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SettingsPage() {
+    return (
+        <div className='mt-10'>
+            <h2 className='text-3xl text-center text-red-500'>Settings Page</h2>
+        </div>
+    )
+}
