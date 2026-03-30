dfd53d4975de8daf8bfff271c511aaec7f9db4ae
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2026-03-30 16:14:37 +0600

    learned about loading and streaming from docs. now understand how loading working in nextjs

---
+++ app/heavy/loading.js
@@ -0,0 +1,5 @@
+export default function HeavyLoading() {
+    return (
+        <div className="text-xl text-center text-red-600">loading.....</div>
+    )
+}
---
+++ app/heavy/page.js
@@ -0,0 +1,10 @@
+export default function HeavyPage() {
+    return (
+        <div className='mtt-10'>
+            <h3 className="text-2xl text-center">This is a Heavy Page</h3>
+            <p className="mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ipsum porro, sunt cupiditate consequuntur dolores, fugit neque debitis culpa totam cumque corporis modi aspernatur doloremque atque non soluta maxime quisquam!
+            </p>
+        </div>
+    )
+}
