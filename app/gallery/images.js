a44abba2da7e16da21b41ed7b4bae5a8e28841ab
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2026-04-06 20:09:48 +0600

    created gallery page using the concept of parallel routes and intercepting

---
+++ app/gallery/images.js
@@ -0,0 +1,47 @@
+import photo1 from "./photos/1.jpg";
+import photo2 from "./photos/2.jpg";
+import photo3 from "./photos/3.jpg";
+import photo4 from "./photos/4.jpg";
+import photo5 from "./photos/5.jpg";
+import photo6 from "./photos/6.jpg";
+import photo7 from "./photos/7.jpg";
+
+const images = [
+    {
+        id: "1",
+        name: "Server Component",
+        src: photo1,
+    },
+    {
+        id: "2",
+        name: "Streaming UI",
+        src: photo2,
+    },
+    {
+        id: "3",
+        name: "NextJS - 14",
+        src: photo3,
+    },
+    {
+        id: "4",
+        name: "Error",
+        src: photo4,
+    },
+    {
+        id: "5",
+        name: "Optimization",
+        src: photo5,
+    },
+    {
+        id: "6",
+        name: "Tailwind",
+        src: photo6,
+    },
+    {
+        id: "7",
+        name: "Deploy",
+        src: photo7,
+    },
+];
+
+export default images;
