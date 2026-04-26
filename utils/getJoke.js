4d6c8c13310d41fee8a7065c5e2e83683c6875ce
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2026-04-26 17:49:33 +0600

    data fetching on server side

---
+++ utils/getJoke.js
@@ -0,0 +1,9 @@
+export default async function getJoke() {
+    const res = await fetch("https://api.chucknorris.io/jokes/random", { cache: 'no-store' });
+
+    if (!res.ok) {
+        throw new Error("Fetch error...");
+    }
+
+    return res.json();
+}
